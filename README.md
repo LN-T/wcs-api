41acc8a375a670bfa099d3c3e29b2a8225cc720d
LN-T <11636138+LN-T@users.noreply.github.com>
2018-02-21 10:13:29 +0100

    first version with drink recipes

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# PHP_Final_Checkpoint
-This repository contains the last PHP checkpoint.
-Open the PDF document called "Final" to see instructions.
-
-Add, commit, and push your project to a new branch with your name, like : LASTNAME_FIRSTNAME. Good luck !
+# Cocktail mix API
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,13 +2,26 @@
 
 namespace App\Controller;
 
+use App\Kernel;
 use GuzzleHttp\Client;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    /**
+     * @var
+     */
+    private $api_absolut;
+
+    /**
+     * DefaultController constructor.
+     */
+    public function __construct()
+    {
+        $this->api_absolut = $_SERVER['API_ABSOLUT'];
+    }
+
     /**
      * @Route("/", name="index")
      */
@@ -16,36 +29,40 @@ class DefaultController extends Controller
     {
 //        $routes = $this->get('router')->getRouteCollection()->all();
 
-        foreach ($this->get('router')->getRouteCollection()->all() as $name => $route) {
-            $routes[] = [$name, $route];
-        }
+//        foreach ($this->get('router')->getRouteCollection()->all() as $name => $route) {
+//            $routes[] = [$name, $route];
+//        }
 
         return $this->render('default/index.html.twig', array(
-            'routes' => $routes
+//            'routes' => $routes
         ));
 
     }
 
     /**
-     * @Route("/getrecipe/", name="getrecipe")
+     * @Route("/getdrink/", name="getdrink")
      */
-    public function getrecipe()
+    public function getdrink()
     {
+        $api_absolut = $_SERVER['API_ABSOLUT'];
+
         $search = $_POST['search'];
-        $url = 'http://food2fork.com/api/search?key=' . '1934704d1230d8fef7f1d866cb96bc7c' . '&q=' . $search;
+
+        $url = 'http://addb.absolutdrinks.com/quickSearch/drinks/' . $search .  '?apiKey=' . $this->api_absolut;
+
         $client = new Client();
         $google = $client->request("GET", $url);
-        $google = json_decode($google->getBody()->getContents());
 
-        $recipes = $google->recipes;
+        $google = json_decode($google->getBody()->getContents(), true);
+        $drinks = $google['result'];
         $error = 'error';
 
-        if($google->count != 0){
-            return $this->render('default/getrecipe.html.twig', array(
-                'recipes' => $recipes
+        if($drinks != null){
+            return $this->render('default/getdrink.html.twig', array(
+                'drinks' => $drinks
             ));
         } else{
-            return $this->render('default/getrecipe.html.twig', array(
+            return $this->render('default/getdrink.html.twig', array(
                 'error' => $error
             ));
         }
@@ -57,53 +74,15 @@ class DefaultController extends Controller
      */
     public function getingredient($id)
     {
-        !empty($_POST['pax']) ? $multiple = $_POST['pax'] : $multiple = 1;
-        $url = 'http://food2fork.com/api/get?key=' . '1934704d1230d8fef7f1d866cb96bc7c' . '&rId=' . $id;
+        $url = 'http://addb.absolutdrinks.com/drinks/' . $id .'/?apiKey=' . $this->api_absolut;
+
         $client = new Client();
         $google = $client->request("GET", $url);
-        $google = json_decode($google->getBody()->getContents());
-        $ingredients = $google->recipe->ingredients;
-
-        foreach($ingredients as $key => $value){
-            $quantity[$key] = explode(' ', $value);
-        }
-        foreach ($quantity as $key2 => $value2) {
-
-            if(isset($value2[0])) {
-                $first_value = $value2[0];
-                $first_value_first = $first_value[0];
-                $first_value_last = $first_value[strlen($first_value) - 1];
-            }
-            if(isset($value2[1])){
-                $second_value = $value2[1];
-                $second_value_first = $second_value[0];
-                $second_value_last = $second_value[strlen($second_value)-1];
-            }
-
-            // 1 chiffre entier
-            if (is_numeric($first_value)){
-                if (is_numeric($second_value_first) && is_numeric($second_value_last)) {
-                    $quantity[$key2] = $first_value + $second_value_first / $second_value_last;
-                }else {
-                    $quantity[$key2] = $first_value;
-                }
-            }
-            // 1 fraction
-            elseif (!is_numeric($first_value) && is_numeric($first_value_first) && is_numeric($first_value_last) ){
-                $quantity[$key2] = $first_value_first/$first_value_last;
-            }
-            // autres cas
-            else{
-                $quantity[$key2] = '';
-            }
-
-        }
-
-        $all = array_combine($ingredients, $quantity);
+        $google = json_decode($google->getBody()->getContents(), true);
+        $drinks = $google['result'];
 
         return $this->render('default/getingredient.html.twig', array(
-            'ingredients' => $all,
-            'multiple' => $multiple,
+            'drinks' => $drinks
         ));
     }
 }
